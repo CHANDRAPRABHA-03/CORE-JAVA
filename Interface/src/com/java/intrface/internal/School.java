ab6ba979cfbf6951495d3151ca69d0e9a8b894af
CHANDRAPRABHA-03 <vchandraprabha903@gmail.com>
2025-04-23 01:04:16 +0530

    Commit

---
+++ Interface/src/com/java/intrface/internal/School.java
@@ -0,0 +1,11 @@
+package com.java.intrface.internal;
+
+public interface School {
+    void Uniforms();
+    void discipline();
+    void Belt();
+    void Tie();
+    void Bag();
+    void Books();
+    void Teachers();
+}
