99b6bb11a4f19147d90fdf4e252b4c1d78a55609
CHANDRAPRABHA-03 <vchandraprabha903@gmail.com>
2025-04-23 01:03:32 +0530

    Commit

---
+++ Interface/src/com/java/intrface/internal/Traffic.java
@@ -0,0 +1,13 @@
+package com.java.intrface.internal;
+
+public interface Traffic {
+    void Vehicles();
+    void TrafficPolice();
+    void redSignal();
+    void OrangeSignal();
+    void GreenSignals();
+    void Fines();
+    void Helmet();
+    void ZebraCross();
+    void SpeedLimit();
+}
