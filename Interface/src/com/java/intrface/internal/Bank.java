72b802297b6e76f3680443a1a3587f46f91456de
CHANDRAPRABHA-03 <vchandraprabha903@gmail.com>
2025-04-23 01:02:29 +0530

    Commit

---
+++ Interface/src/com/java/intrface/internal/Bank.java
@@ -0,0 +1,13 @@
+package com.java.intrface.internal;
+
+public interface Bank {
+    void Amount();
+    void Customer();
+    void Security();
+    void Atm();
+    void BankManager();
+    void BankEmployee();
+    void issueLoan();
+    void openAccount();
+    void Loyal();
+}
