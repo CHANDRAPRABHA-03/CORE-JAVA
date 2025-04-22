68403e2948d9ab6e6fae275aabeed9e35d2d6eee
CHANDRAPRABHA-03 <vchandraprabha903@gmail.com>
2025-04-23 01:03:01 +0530

    Commit

---
+++ Interface/src/com/java/intrface/internal/Election.java
@@ -0,0 +1,13 @@
+package com.java.intrface.internal;
+
+public interface Election {
+    void Candidate();
+    void Machine();
+    void Ink();
+    void Place();
+    void SecurityPolice();
+    void CCcamera();
+    void VoterId();
+    void ageLimit();
+    void Results();
+}
