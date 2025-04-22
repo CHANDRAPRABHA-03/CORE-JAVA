859cd497c878be04c454614f40fbea31ef4470a4
CHANDRAPRABHA-03 <vchandraprabha903@gmail.com>
2025-04-23 01:03:40 +0530

    Commit

---
+++ Interface/src/com/java/intrface/internal/Office.java
@@ -0,0 +1,11 @@
+package com.java.intrface.internal;
+
+public interface Office {
+    void Laptop();
+    void Employee();
+    void Manager();
+    void HR();
+    void Accountant();
+    void Apartment();
+    void ITSupport();
+}
