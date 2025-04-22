fddd6a566aaff14bbe4307bad3d714c6c483f4da
CHANDRAPRABHA-03 <vchandraprabha903@gmail.com>
2025-04-23 01:04:20 +0530

    Commit

---
+++ Interface/src/com/java/intrface/internal/PoliceStation.java
@@ -0,0 +1,12 @@
+package com.java.intrface.internal;
+
+public interface PoliceStation {
+    void Police();
+    void Constable();
+    void Jail();
+    void Uniform();
+    void PublicSafety();
+    void punctuality();
+    void Shoes();
+
+}
