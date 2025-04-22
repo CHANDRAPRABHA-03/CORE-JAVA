d42ca4c46fb558d4c8ec237342410c206375485d
CHANDRAPRABHA-03 <vchandraprabha903@gmail.com>
2025-04-23 01:04:34 +0530

    Commit

---
+++ Interface/src/com/java/intrface/internal/SevenMethod.java
@@ -0,0 +1,179 @@
+package com.java.intrface.internal;
+
+public class SevenMethod implements PoliceStation, RailwayStation, School, Society, Office {
+
+    @Override
+    public void Laptop() {
+        System.out.println("Laptop is in Office");
+    }
+
+    @Override
+    public void Employee() {
+        System.out.println("Employee is in Office");
+    }
+
+    @Override
+    public void Manager() {
+        System.out.println("Manager is in Office");
+    }
+
+    @Override
+    public void HR() {
+        System.out.println("HR is in Office");
+    }
+
+    @Override
+    public void Accountant() {
+        System.out.println("Accountant is in Office");
+    }
+
+    @Override
+    public void Apartment() {
+        System.out.println("Apartment is in Society");
+    }
+
+    @Override
+    public void ITSupport() {
+        System.out.println("ITSupport is in Society");
+    }
+
+    @Override
+    public void Police() {
+        System.out.println("Police is in PoliceStation");
+    }
+
+    @Override
+    public void Constable() {
+        System.out.println("Constable is in PoliceStation");
+    }
+
+    @Override
+    public void Jail() {
+        System.out.println("Jail is in PoliceStation");
+    }
+
+    @Override
+    public void Uniform() {
+        System.out.println("Uniform is in PoliceStation");
+    }
+
+    @Override
+    public void PublicSafety() {
+        System.out.println("PublicSafety is in PoliceStation");
+    }
+
+    @Override
+    public void punctuality() {
+        System.out.println("punctuality is in School");
+    }
+
+    @Override
+    public void Shoes() {
+        System.out.println("Shoes are in School");
+    }
+
+    @Override
+    public void ticket() {
+        System.out.println("ticket is in RailwayStation");
+    }
+
+    @Override
+    public void Train() {
+        System.out.println("Train is in RailwayStation");
+    }
+
+    @Override
+    public void Passenger() {
+        System.out.println("Passenger is in RailwayStation");
+    }
+
+    @Override
+    public void Toilets() {
+        System.out.println("Toilets are in RailwayStation");
+    }
+
+    @Override
+    public void Seats() {
+        System.out.println("Seats are in RailwayStation");
+    }
+
+    @Override
+    public void SleeperCoach() {
+        System.out.println("SleeperCoach is in RailwayStation");
+    }
+
+    @Override
+    public void General() {
+        System.out.println("General coach is in RailwayStation");
+    }
+
+    @Override
+    public void Uniforms() {
+        System.out.println("Uniforms are in School");
+    }
+
+    @Override
+    public void discipline() {
+        System.out.println("discipline is in School");
+    }
+
+    @Override
+    public void Belt() {
+        System.out.println("Belt is in School");
+    }
+
+    @Override
+    public void Tie() {
+        System.out.println("Tie is in School");
+    }
+
+    @Override
+    public void Bag() {
+        System.out.println("Bag is in School");
+    }
+
+    @Override
+    public void Books() {
+        System.out.println("Books are in School");
+    }
+
+    @Override
+    public void Teachers() {
+        System.out.println("Teachers are in School");
+    }
+
+    @Override
+    public void PromoteEducation() {
+        System.out.println("PromoteEducation is in School");
+    }
+
+    @Override
+    public void Job() {
+        System.out.println("Job is in Society");
+    }
+
+    @Override
+    public void Marriage() {
+        System.out.println("Marriage is in Society");
+    }
+
+    @Override
+    public void Equality() {
+        System.out.println("Equality is in Society");
+    }
+
+    @Override
+    public void Safety() {
+        System.out.println("Safety is in Society");
+    }
+
+    @Override
+    public void EconomicGrowth() {
+        System.out.println("EconomicGrowth is in Society");
+    }
+
+    @Override
+    public void HealthCare() {
+        System.out.println("HealthCare is in Society");
+    }
+}
