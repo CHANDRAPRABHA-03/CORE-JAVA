85e13187a2e7d6c5822a0601be4cfc32c5e79121
CHANDRAPRABHA-03 <vchandraprabha903@gmail.com>
2025-04-23 01:04:12 +0530

    Commit

---
+++ Interface/src/com/java/intrface/internal/ScndMethod.java
@@ -0,0 +1,78 @@
+package com.java.intrface.internal;
+
+public class ScndMethod implements Company,Court,Football,Home,Hospital{
+    @Override
+    public void LoginTime() {
+        System.out.println("LoginTime is in SecondMethod");
+    }
+
+    @Override
+    public void Exit() {
+        System.out.println("Exit is in SecondMethod");
+    }
+
+    @Override
+    public void Work() {
+        System.out.println("Work is in SecondMethod");
+    }
+
+    @Override
+    public void Judgement() {
+        System.out.println("Judgement is in SecondMethod");
+    }
+
+    @Override
+    public void Promise() {
+        System.out.println("Promise is in SecondMethod");
+    }
+
+    @Override
+    public void Lawyer() {
+        System.out.println("Lawyer is in SecondMethod");
+    }
+
+    @Override
+    public void Goal() {
+        System.out.println("Goal is in SecondMethod");
+    }
+
+    @Override
+    public void Ball() {
+        System.out.println("Ball is in SecondMethod");
+    }
+
+    @Override
+    public void Shoe() {
+        System.out.println("Shoe is in SecondMethod");
+    }
+
+    @Override
+    public void Time() {
+        System.out.println("Time is in SecondMethod");
+    }
+
+    @Override
+    public void Food() {
+        System.out.println("Food is in SecondMethod");
+    }
+
+    @Override
+    public void Wakeup() {
+        System.out.println("Wakeup is in SecondMethod");
+    }
+
+    @Override
+    public void patient() {
+        System.out.println("patient is in SecondMethod");
+    }
+
+    @Override
+    public void Doctor() {
+        System.out.println("Doctor is in SecondMethod");
+    }
+
+    @Override
+    public void Nurse() {
+        System.out.println("Nurse is in SecondMethod");
+    }
+}
