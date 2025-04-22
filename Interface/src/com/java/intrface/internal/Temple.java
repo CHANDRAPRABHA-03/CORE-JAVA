0e7ffa93ab3570b346dbf0c55b07b4ee18fbf180
CHANDRAPRABHA-03 <vchandraprabha903@gmail.com>
2025-04-23 01:03:36 +0530

    Commit

---
+++ Interface/src/com/java/intrface/internal/Temple.java
@@ -0,0 +1,13 @@
+package com.java.intrface.internal;
+
+public interface Temple {
+    void God();
+    void Prasada();
+    void Parking();
+    void SlipperOut();
+    void Queue();
+    void Bell();
+    void Flower();
+    void Prayer();
+    void Donation();
+}
