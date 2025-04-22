8d6d9b66bbc05a8e3e34f64c15d7543f9402b96e
CHANDRAPRABHA-03 <vchandraprabha903@gmail.com>
2025-04-23 01:03:08 +0530

    Commit

---
+++ Interface/src/com/java/intrface/internal/FiveMethod.java
@@ -0,0 +1,128 @@
+package com.java.intrface.internal;
+
+public class FiveMethod implements Hostel,Hotel,Library,Metro,Mosque{
+    @Override
+    public void Timings() {
+        System.out.println("Timings Is in FiveMethod");
+    }
+
+    @Override
+    public void Warden() {
+        System.out.println("Warden Is in FiveMethod");
+    }
+
+    @Override
+    public void Bed() {
+        System.out.println("Bed Is in FiveMethod");
+    }
+
+    @Override
+    public void Locker() {
+        System.out.println("Locker Is in FiveMethod");
+    }
+
+    @Override
+    public void security() {
+        System.out.println("security Is in FiveMethod");
+    }
+
+    @Override
+    public void Parking() {
+        System.out.println("Parking Is in FiveMethod");
+    }
+
+    @Override
+    public void GoodFood() {
+        System.out.println("GoodFood Is in FiveMethod");
+    }
+
+    @Override
+    public void cleaning() {
+        System.out.println("cleaning Is in FiveMethod");
+    }
+
+    @Override
+    public void Chef() {
+        System.out.println("Chef Is in FiveMethod");
+    }
+
+    @Override
+    public void Menu() {
+        System.out.println("Menu Is in FiveMethod");
+    }
+
+    @Override
+    public void Silence() {
+        System.out.println("Silence Is in FiveMethod");
+    }
+
+    @Override
+    public void Books() {
+        System.out.println("Books Is in FiveMethod");
+    }
+
+    @Override
+    public void Librarian() {
+        System.out.println("Librarian Is in FiveMethod");
+    }
+
+    @Override
+    public void LibraryCard() {
+        System.out.println("LibraryCard Is in FiveMethod");
+    }
+
+    @Override
+    public void Newspaper() {
+        System.out.println("Newspaper Is in FiveMethod");
+    }
+
+    @Override
+    public void line() {
+        System.out.println("line Is in FiveMethod");
+    }
+
+    @Override
+    public void card() {
+        System.out.println("card Is in FiveMethod");
+    }
+
+    @Override
+    public void Lift() {
+        System.out.println("Lift Is in FiveMethod");
+    }
+
+    @Override
+    public void Announcement() {
+        System.out.println("Announcement Is in FiveMethod");
+    }
+
+    @Override
+    public void ScanCard() {
+        System.out.println("ScanCard Is in FiveMethod");
+    }
+
+    @Override
+    public void Gents() {
+        System.out.println("Gents Is in FiveMethod");
+    }
+
+    @Override
+    public void PrayerMat() {
+        System.out.println("PrayerMat Is in FiveMethod");
+    }
+
+    @Override
+    public void quran() {
+        System.out.println("quran Is in FiveMethod");
+    }
+
+    @Override
+    public void Ramdan() {
+        System.out.println("Ramdan Is in FiveMethod");
+    }
+
+    @Override
+    public void Shall() {
+        System.out.println("Shall Is in FiveMethod");
+    }
+}
