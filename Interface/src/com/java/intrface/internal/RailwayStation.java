3fb3ecb34ab528e2ae990af12a9d4b935906805d
CHANDRAPRABHA-03 <vchandraprabha903@gmail.com>
2025-04-23 01:04:38 +0530

    Commit

---
+++ Interface/src/com/java/intrface/internal/RailwayStation.java
@@ -0,0 +1,11 @@
+package com.java.intrface.internal;
+
+public interface RailwayStation {
+    void ticket();
+    void Train();
+    void Passenger();
+    void Toilets();
+    void Seats();
+    void SleeperCoach();
+    void General();
+}
