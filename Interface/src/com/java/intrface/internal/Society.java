8c5a8d7b6f3526cc9f37065de7b19141a3fa164b
CHANDRAPRABHA-03 <vchandraprabha903@gmail.com>
2025-04-23 01:04:42 +0530

    Commit

---
+++ Interface/src/com/java/intrface/internal/Society.java
@@ -0,0 +1,12 @@
+package com.java.intrface.internal;
+
+public interface Society {
+    void PromoteEducation();
+    void Job();
+    void Marriage();
+    void Equality();
+    void Safety();
+    void EconomicGrowth();
+    void HealthCare();
+
+}
