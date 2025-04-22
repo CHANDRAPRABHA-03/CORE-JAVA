20e048b614b9d0881970d9a7d71063c263efa370
CHANDRAPRABHA-03 <vchandraprabha903@gmail.com>
2025-04-23 01:03:28 +0530

    Commit

---
+++ Interface/src/com/java/intrface/internal/NineMethods.java
@@ -0,0 +1,229 @@
+package com.java.intrface.internal;
+
+public class NineMethods implements Volleyball, Traffic, Temple, Election, Bank {
+
+    @Override
+    public void Amount() {
+        System.out.println("Amount is in NineMethods");
+    }
+
+    @Override
+    public void Customer() {
+        System.out.println("Customer is in NineMethods");
+    }
+
+    @Override
+    public void Security() {
+        System.out.println("Security is in NineMethods");
+    }
+
+    @Override
+    public void Atm() {
+        System.out.println("ATM is in NineMethods");
+    }
+
+    @Override
+    public void BankManager() {
+        System.out.println("BankManager is in NineMethods");
+    }
+
+    @Override
+    public void BankEmployee() {
+        System.out.println("BankEmployee is in NineMethods");
+    }
+
+    @Override
+    public void issueLoan() {
+        System.out.println("IssueLoan is in NineMethods");
+    }
+
+    @Override
+    public void openAccount() {
+        System.out.println("OpenAccount is in NineMethods");
+    }
+
+    @Override
+    public void Loyal() {
+        System.out.println("Loyal is in NineMethods");
+    }
+
+    @Override
+    public void Candidate() {
+        System.out.println("Candidate is in NineMethods");
+    }
+
+    @Override
+    public void Machine() {
+        System.out.println("Machine is in NineMethods");
+    }
+
+    @Override
+    public void Ink() {
+        System.out.println("Ink is in NineMethods");
+    }
+
+    @Override
+    public void Place() {
+        System.out.println("Place is in NineMethods");
+    }
+
+    @Override
+    public void SecurityPolice() {
+        System.out.println("SecurityPolice is in NineMethods");
+    }
+
+    @Override
+    public void CCcamera() {
+        System.out.println("CCcamera is in NineMethods");
+    }
+
+    @Override
+    public void VoterId() {
+        System.out.println("VoterId is in NineMethods");
+    }
+
+    @Override
+    public void ageLimit() {
+        System.out.println("AgeLimit is in NineMethods");
+    }
+
+    @Override
+    public void Results() {
+        System.out.println("Results is in NineMethods");
+    }
+
+    @Override
+    public void God() {
+        System.out.println("God is in NineMethods");
+    }
+
+    @Override
+    public void Prasada() {
+        System.out.println("Prasada is in NineMethods");
+    }
+
+    @Override
+    public void Parking() {
+        System.out.println("Parking is in NineMethods");
+    }
+
+    @Override
+    public void SlipperOut() {
+        System.out.println("SlipperOut is in NineMethods");
+    }
+
+    @Override
+    public void Queue() {
+        System.out.println("Queue is in NineMethods");
+    }
+
+    @Override
+    public void Bell() {
+        System.out.println("Bell is in NineMethods");
+    }
+
+    @Override
+    public void Flower() {
+        System.out.println("Flower is in NineMethods");
+    }
+
+    @Override
+    public void Prayer() {
+        System.out.println("Prayer is in NineMethods");
+    }
+
+    @Override
+    public void Donation() {
+        System.out.println("Donation is in NineMethods");
+    }
+
+    @Override
+    public void Vehicles() {
+        System.out.println("Vehicles is in NineMethods");
+    }
+
+    @Override
+    public void TrafficPolice() {
+        System.out.println("TrafficPolice is in NineMethods");
+    }
+
+    @Override
+    public void redSignal() {
+        System.out.println("RedSignal is in NineMethods");
+    }
+
+    @Override
+    public void OrangeSignal() {
+        System.out.println("OrangeSignal is in NineMethods");
+    }
+
+    @Override
+    public void GreenSignals() {
+        System.out.println("GreenSignals is in NineMethods");
+    }
+
+    @Override
+    public void Fines() {
+        System.out.println("Fines is in NineMethods");
+    }
+
+    @Override
+    public void Helmet() {
+        System.out.println("Helmet is in NineMethods");
+    }
+
+    @Override
+    public void ZebraCross() {
+        System.out.println("ZebraCross is in NineMethods");
+    }
+
+    @Override
+    public void SpeedLimit() {
+        System.out.println("SpeedLimit is in NineMethods");
+    }
+
+    @Override
+    public void Court() {
+        System.out.println("Court is in NineMethods");
+    }
+
+    @Override
+    public void Ball() {
+        System.out.println("Ball is in NineMethods");
+    }
+
+    @Override
+    public void Players() {
+        System.out.println("Players is in NineMethods");
+    }
+
+    @Override
+    public void Net() {
+        System.out.println("Net is in NineMethods");
+    }
+
+    @Override
+    public void Teams() {
+        System.out.println("Teams is in NineMethods");
+    }
+
+    @Override
+    public void Captain() {
+        System.out.println("Captain is in NineMethods");
+    }
+
+    @Override
+    public void Strength() {
+        System.out.println("Strength is in NineMethods");
+    }
+
+    @Override
+    public void Serve() {
+        System.out.println("Serve is in NineMethods");
+    }
+
+    @Override
+    public void Umpires() {
+        System.out.println("Umpires is in NineMethods");
+    }
+}
