0dfad77494191042047d390b41cb09d8f1c95566
CHANDRAPRABHA-03 <vchandraprabha903@gmail.com>
2025-04-23 01:04:27 +0530

    Commit

---
+++ Interface/src/com/java/intrface/internal/Volleyball.java
@@ -0,0 +1,13 @@
+package com.java.intrface.internal;
+
+public interface Volleyball {
+    void Court();
+    void Ball();
+    void Players();
+    void Net();
+    void Teams();
+    void Captain();
+    void Strength();
+    void Serve();
+    void Umpires();
+}
